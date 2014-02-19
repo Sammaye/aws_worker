4bcfc5790726e310f8da6017141a7e9da7a508a6
Sammaye <sam.millman@googlemail.com>
2014-02-19 00:08:51 +0000

    Added better ogg handling

--- encoder.php
+++ encoder.php
@@ -82,11 +82,12 @@ if(!empty($output)){
  * LETS ENCODE!!!!
  */
 if($args['output_format'] == 'mp4'){
-	//-b:v 5K 
+	// -b:v 5K
 	$command = APP_ROOT . "ffmpeg -i $input_file_name -vprofile baseline -vcodec libx264 -r 48 -s 960x540 -ac 2 -ar 48000 -ab 192k -strict -2 -y $output_temp_file 2>&1";
 	//$command = APP_ROOT."ffmpeg -i $input_file_name -vcodec libx264 -r 100 -bt 300k -ac 2 -ar 48000 -ab 192k -strict -2 -y $output_temp_file 2>&1";
 }elseif($args['output_format'] == 'ogv'){
-	$command = APP_ROOT."ffmpeg -i $input_file_name -s 640:480 -acodec libvorbis -vcodec libtheora -aspect 4:3 -r 20 -qscale 6 -ac 2 -ab 80k -ar 44100 -y $output_file_name 2>&1";
+	$command = APP_ROOT."ffmpeg -i $input_file_name -s 960:540 -acodec libvorbis -vcodec libtheora -aspect 16:9 -r 40 -qscale 6 -ac 2 -ab 80k -ar 44100 -y $output_file_name 2>&1";
+	//$command = APP_ROOT."ffmpeg -i $input_file_name -s 640:480 -acodec libvorbis -vcodec libtheora -aspect 4:3 -r 20 -qscale 6 -ac 2 -ab 80k -ar 44100 -y $output_file_name 2>&1";
 }elseif($args['output_format'] == 'img'){
 	/*
 	 * From our duration we got earlier lets get a random second between 0 and max second without rounding and check the image file is real by chekcing its size is greater
